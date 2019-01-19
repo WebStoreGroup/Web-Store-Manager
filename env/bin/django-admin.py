05a60e5e7b0da094c2e98fb079fb171cba4aadec
Jonathan Christopher Jakub <jonathanchristopher1199@gmail.com>
2019-01-19 23:08:27 +0700

    add env

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/jonathanjojo/Documents/Web Store Manager/Store_Manager/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
