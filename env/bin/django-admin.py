0689577539abb860a8fc418098799dd5008426b1
davenathanael <davenathanaeldev@gmail.com>
2019-01-20 17:55:11 +0700

    Removed env

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/dep/Development/belajar/web-js/web-store/Web-Store-Manager/env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
